3a52534cc93d8024ea275a3e8f35af4f7260a31d
Dwintermeier <144943052+Dwintermeier@users.noreply.github.com>
2023-09-13 14:52:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,34 +9,23 @@ def get_table_download_link(df):
     in:  dataframe
     out: href string
     """
-    # Get current date
     current_date = datetime.datetime.now().strftime('%m-%d-%y')
-
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="PPV_{current_date}.csv">Download csv file</a>'
     return href
-    
+
 # Upload button for Excel file
 uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
 
 if uploaded_file:
-    # Read the Excel file into a DataFrame
     df = pd.read_excel(uploaded_file, skiprows=1)
-    
-    # Remove columns: AH, AI, AJ, AC, and AD
     columns_to_remove = ['AH', 'AI', 'AJ', 'AC', 'AD']
     df.drop(columns=columns_to_remove, errors='ignore', inplace=True)
-    
-    # Delete all rows where values in column E contain "PAAS"
     df = df[df['Part Profit Center Profit Center'] != 'PAAS']
-    
-    # Remove column E
     df.drop(columns=['Part Profit Center Profit Center'], errors='ignore', inplace=True)
-    
-    # Delete all rows where value in column H contain "06-LE/FC-N"
     df = df[df['Value'] != '06-LE/FC-N']
-    
+
     # Additional Cleansing Steps
     df.loc[df['Des'] == 'Purch Req', 'Supply Source'] = 'Purch Req'
     df.loc[df['Des'] == 'Sched Agrmt', 'Supply Source'] = 'Sched Agrmt'
@@ -44,6 +33,8 @@ if uploaded_file:
     df = df[df['Supply Source'] != 'SubstituteSupply']
     df.drop(columns=['Des', 'Value'], errors='ignore', inplace=True)
     
-    # Export to CSV (as a Download Link)
+    # Commenting out the DataFrame display to improve performance
+    # st.write('Cleansed Data')
+    # st.write(df)
+    
     st.markdown(get_table_download_link(df), unsafe_allow_html=True)
-
