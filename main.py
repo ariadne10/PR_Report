e04d530237928a020e51d904b9808821cf2d9f98
Dwintermeier <144943052+Dwintermeier@users.noreply.github.com>
2023-09-13 15:53:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -20,34 +20,38 @@ uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
 
 if uploaded_file:
     df = pd.read_excel(uploaded_file, skiprows=1)
-    columns_to_remove = ['Buyer Name', 'Global Name', 'Supplier Name', 'Total Nettable On Hand', 'Net Req', 'Action']
+
+    # Initial Cleansing Steps
+    columns_to_remove = ['Buyer Name', 'Global Name', 'Supplier Name', 'Total Nettable On Hand', 'Net Req']
     df.drop(columns=columns_to_remove, errors='ignore', inplace=True)
     df = df[df['Part Profit Center Profit Center'] != 'PAAS']
     df.drop(columns=['Part Profit Center Profit Center'], errors='ignore', inplace=True)
     df = df[df['Value'] != '06-LE/FC-N']
-
+    
     # Additional Cleansing Steps
-    df.loc[df['Des'] == 'Purch Req', 'Supply Source'] = 'Purch Req'
-    df.loc[df['Des'] == 'Sched Agrmt', 'Supply Source'] = 'Sched Agrmt'
-    df.loc[df['Des'] == 'Firm Planned Order', 'Supply Source'] = 'PlannedOrder'
+    df.loc[df['Des'].str.contains('Purch Req', na=False), 'Supply Source'] = 'Purch Req'
+    df.loc[df['Des'].str.contains('Sched Agrmt', na=False), 'Supply Source'] = 'Sched Agrmt'
+    df.loc[df['Des'].str.contains('Firm Planned Order', na=False), 'Supply Source'] = 'PlannedOrder'
     df = df[df['Supply Source'] != 'SubstituteSupply']
     df.drop(columns=['Des', 'Value'], errors='ignore', inplace=True)
+
+    # New Cleansing Steps
+    # Convert 'Date Release' to datetime format and 'GRPT' to numeric if they are not
+    df['Date Release'] = pd.to_datetime(df['Date Release'])
+    df['GRPT'] = pd.to_numeric(df['GRPT'], errors='coerce')
     
-# Convert 'Date Release' to datetime format and 'GRPT' to numeric if they are not
-df['Date Release'] = pd.to_datetime(df['Date Release'])
-df['GRPT'] = pd.to_numeric(df['GRPT'], errors='coerce')
+    # Perform the date subtraction
+    df['Date Release1'] = df['Date Release'] - pd.to_timedelta(df['GRPT'], unit='D')
 
-# Perform the date subtraction
-df['Date Release1'] = df['Date Release'] - pd.to_timedelta(df['GRPT'], unit='D')
+    # Drop the original columns
+    df.drop(columns=['GRPT', 'Date Release', 'Action'], errors='ignore', inplace=True)
 
-# Drop the original columns
-df.drop(columns=['GRPT', 'Date Release'], errors='ignore', inplace=True)
-
-# Rename the new column
-df.rename(columns={'Date Release1': 'Date Release'}, inplace=True)
-   
+    # Rename the new column
+    df.rename(columns={'Date Release1': 'Date Release'}, inplace=True)
+    
     # Commenting out the DataFrame display to improve performance
     # st.write('Cleansed Data')
     # st.write(df)
     
-st.markdown(get_table_download_link(df), unsafe_allow_html=True)
+    # Download button for cleansed data
+    st.markdown(get_table_download_link(df), unsafe_allow_html=True)
