1efad80cddcfac783f3751855837141ab93b0cde
Dwintermeier <144943052+Dwintermeier@users.noreply.github.com>
2023-09-14 14:57:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def get_table_download_link(df):
     href = f'<a href="data:file/csv;base64,{b64}" download="PPV_{current_date}.csv">Download csv file</a>'
     return href
 
+# File uploaders for the two different Excel files
 uploaded_file = st.file_uploader("Choose the main Excel file", type="xlsx")
 uploaded_file2 = st.file_uploader("Choose the 'S72 Sites and PICs' Excel file", type="xlsx")
 
@@ -17,40 +18,47 @@ if uploaded_file and uploaded_file2:
     df = pd.read_excel(uploaded_file, skiprows=1)
     df2 = pd.read_excel(uploaded_file2)
 
-    st.write(f"Columns in main file: {df.columns.tolist()}")
-    st.write(f"Columns in 'S72 Sites and PICs' file: {df2.columns.tolist()}")
-    
-    # Initial data cleansing
+    # Initial data cleansing for the main file
     columns_to_remove = ['Buyer Name', 'Global Name', 'Supplier Name', 'Total Nettable On Hand', 'Net Req']
     df.drop(columns=columns_to_remove, errors='ignore', inplace=True)
     df = df[df['Part Profit Center Profit Center'] != 'PAAS']
     df.drop(columns=['Part Profit Center Profit Center'], errors='ignore', inplace=True)
     df = df[df['Value'] != '06-LE/FC-N']
-
-    # Additional Cleansing Steps
-    df.loc[df['Des'] == 'Purch Req', 'Supply Source'] = 'Purch Req'
-    df.loc[df['Des'] == 'Sched Agrmt', 'Supply Source'] = 'Sched Agrmt'
-    df.loc[df['Des'] == 'Firm Planned Order', 'Supply Source'] = 'PlannedOrder'
-    df = df[df['Supply Source'] != 'SubstituteSupply']
     df.drop(columns=['Des', 'Value', 'Action', 'Indep Dmnd'], errors='ignore', inplace=True)
-
     df = df.loc[pd.to_datetime(df['Date Release'], errors='coerce').notna()]
     df['Date Release'] = pd.to_datetime(df['Date Release'])
     df['Date Release1'] = df['Date Release'] - pd.to_timedelta(df['GRPT'], unit='D')
     df.drop(columns=['GRPT', 'Date Release'], errors='ignore', inplace=True)
     df.rename(columns={'Date Release1': 'Date Release'}, inplace=True)
 
-  # Add CONC column
+    # Additional calculations
+    df['1'] = df['Total Dmnd'] - df['Net OH']
+    df['2'] = df['1'] >= df['PR Qty']
+    df['3'] = df['1'] * df['Std Price']
+
+    # Reorder the columns
+    col_names = df.columns.tolist()
+    date_rel_index = col_names.index('Date Release')
+    deliv_date_index = col_names.index('Delivery Date')
+    col_names.insert(deliv_date_index, col_names.pop(date_rel_index))
+    df = df[col_names]
+
+    # More data manipulations
+    df['Delivery Date'] = pd.to_datetime(df['Delivery Date']).dt.date
+    df = df[df['3'] >= 500]
+    df.loc[df['2'] == False, 'PR Qty'] = df['1']
+    df.drop(columns=['1', '2', '3'], inplace=True)
+
+    # Add the 'CONC' column
     df['CONC'] = df['Site Code'].astype(str) + df['BU Name'].astype(str)
 
-    # Get values from columns L and M in 'S72 Sites and PICs' file
+    # Check for columns 12 and 13 in the 'S72 Sites and PICs' file
     try:
         remove_values = df2.loc[df2.iloc[:, 13] == '** Remove **', df2.columns[12]]
     except IndexError:
-        st.write("Error: Columns L and M not found in 'S72 Sites and PICs' file.")
+        st.write("Error: Columns 12 and 13 not found in 'S72 Sites and PICs' file.")
     else:
-        # Remove rows from the main dataframe based on CONC column
+        # Remove rows from the main DataFrame based on CONC column
         df = df[~df['CONC'].isin(remove_values)]
 
-    # Generate and display download link
     st.markdown(get_table_download_link(df), unsafe_allow_html=True)
