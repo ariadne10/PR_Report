ffad9963f28628bf945569a4911dbaab5c700dbe
Dwintermeier <144943052+Dwintermeier@users.noreply.github.com>
2023-09-14 08:19:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,51 +4,31 @@ import base64
 import datetime
 
 def get_table_download_link(df):
-    """Generates a link allowing the data in a given panda dataframe to be downloaded
-    in:  dataframe
-    out: href string
-    """
     current_date = datetime.datetime.now().strftime('%m-%d-%y')
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="PPV_{current_date}.csv">Download csv file</a>'
     return href
 
-# Upload button for Excel file
 uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
 
 if uploaded_file:
     df = pd.read_excel(uploaded_file, skiprows=1)
-
-    # Initial data cleansing
     columns_to_remove = ['Buyer Name', 'Global Name', 'Supplier Name', 'Total Nettable On Hand', 'Net Req']
     df.drop(columns=columns_to_remove, errors='ignore', inplace=True)
     df = df[df['Part Profit Center Profit Center'] != 'PAAS']
     df.drop(columns=['Part Profit Center Profit Center'], errors='ignore', inplace=True)
     df = df[df['Value'] != '06-LE/FC-N']
-
-    # Additional Cleansing Steps
     df.loc[df['Des'] == 'Purch Req', 'Supply Source'] = 'Purch Req'
     df.loc[df['Des'] == 'Sched Agrmt', 'Supply Source'] = 'Sched Agrmt'
     df.loc[df['Des'] == 'Firm Planned Order', 'Supply Source'] = 'PlannedOrder'
     df = df[df['Supply Source'] != 'SubstituteSupply']
     df.drop(columns=['Des', 'Value', 'Action'], errors='ignore', inplace=True)
 
-    # Date manipulation
-    try:
-        df['Date Release'] = pd.to_datetime(df['Date Release'])
-        df['Date Release1'] = df['Date Release'] - pd.to_timedelta(df['GRPT'], unit='D')
-    except Exception as e:
-        problematic_values = df.loc[pd.to_datetime(df['Date Release'], errors='coerce').isna(), 'Date Release']
-        st.write("Found problematic values in 'Date Release':", problematic_values)
-        st.write("Error:", str(e))
-        raise e
+    # Drop rows where 'Date Release' is null
+    df = df.dropna(subset=['Date Release'])
 
-    # Remove 'GRPT' and 'Date Release'
-    df.drop(columns=['GRPT', 'Date Release'], errors='ignore', inplace=True)
-    
-    # Rename 'Date Release1' to 'Date Release'
-    df.rename(columns={'Date Release1': 'Date Release'}, inplace=True)
-    
-    # Download button
+    df['Date Release'] = pd.to_datetime(df['Date Release'])
+    df['Date Release1'] = df['Date Release'] - pd.to_timedelta(df['GRPT'], unit='D')
+
     st.markdown(get_table_download_link(df), unsafe_allow_html=True)
